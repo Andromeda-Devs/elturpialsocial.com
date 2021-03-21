f2ef6f1d3d6654a7c398f1eddf93988df782f14e
jesus ortiz <ortizjesusrivas@gmail.com>
2021-03-20 22:28:24 -0400

    wip

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 const app = require("./app");
-const models = require("./models");
-
 app.listen(app.get("port"));
-models.sequelize.sync({ force: false }).then(() => {
-	console.log("success database");
-});
 console.log("Server on port", app.get("port"));
